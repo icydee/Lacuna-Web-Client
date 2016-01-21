fbead5506c9cf280dfdfba1d95a6c0f4d5b0f373
Iain C Docherty <iain@iain-docherty.com>
2016-01-21 15:58:18 -0500

    Make map control trinary.

--- app/js/actions/menu/map.js
+++ app/js/actions/menu/map.js
@@ -5,6 +5,7 @@ var Reflux = require('reflux');
 var MapActions = Reflux.createActions([
     'showPlanetMap',
     'showStarMap',
+    'showNewMap',
     'toggleMapMode',
     'changePlanet'
 ]);
--- app/js/stores/menu/mapMode.js
+++ app/js/stores/menu/mapMode.js
@@ -5,7 +5,8 @@ var Reflux = require('reflux');
 var MapActions = require('js/actions/menu/map');
 
 var PLANET_MAP_MODE = 'planetMap';
-var STAR_MAP_MODE = 'starMap';
+var STAR_MAP_MODE   = 'starMap';
+var NEW_MAP_MODE    = 'newMap';
 
 var MapModeStore = Reflux.createStore({
     listenables: MapActions,
@@ -33,11 +34,24 @@ var MapModeStore = Reflux.createStore({
         this.setMapMode(STAR_MAP_MODE);
     },
 
+    onShowNewMap: function() {
+        this.setMapMode(NEW_MAP_MODE);
+    },
+
     onToggleMapMode: function() {
-        this.setMapMode(this.mapMode === PLANET_MAP_MODE ? STAR_MAP_MODE : PLANET_MAP_MODE);
+        if (this.mapMode === PLANET_MAP_MODE) {
+            this.setMapMode(STAR_MAP_MODE);
+        }
+        else if (this.mapMode === STAR_MAP_MODE) {
+            this.setMapMode(NEW_MAP_MODE);
+        }
+        else {
+            this.setMapMode(PLANET_MAP_MODE);
+        }
     }
 });
 
 module.exports = MapModeStore;
-module.exports.PLANET_MAP_MODE = PLANET_MAP_MODE;
-module.exports.STAR_MAP_MODE = STAR_MAP_MODE;
+module.exports.PLANET_MAP_MODE  = PLANET_MAP_MODE;
+module.exports.STAR_MAP_MODE    = STAR_MAP_MODE;
+module.exports.NEW_MAP_MODE     = NEW_MAP_MODE;
