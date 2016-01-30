1d8859497cb1579f1cb2257eb30f277421089c05
Iain C Docherty <iain@iain-docherty.com>
2016-01-30 05:40:06 -0500

    Preliminary code, can now drag star-map

--- app/js/components/window/starfield.jsx
+++ app/js/components/window/starfield.jsx
@@ -1,18 +1,34 @@
 'use strict';
 
-var React = require('react');
-var Reflux = require('reflux');
+var React       = require('react');
+var Reflux      = require('reflux');
+var Draggable   = require('react-draggable');
+var Panel       = require('js/components/panel');
 
-var Panel = require('js/components/panel');
 var Lib = window.YAHOO.lacuna.Library;
 var starFieldImage = "url(" + Lib.AssetUrl+'star_system/field.png' + ")";
 
+
 var StarField = React.createClass({
-
-
     render: function() {
         return (
-            <div id="starmap" style={{ width: "100vw", height: "100vh", position: "absolute", top: 0, backgroundImage: starFieldImage }} >
+            <div id="expanse" style={{
+                width:      "100vw",
+                height:     "100vh",
+                position:   "absolute",
+                top:        0,
+                left:       0
+              }}>
+              <Draggable >
+                <div id="starmap" style={{ 
+                  width:    20000, 
+                  height:   20000, 
+                  position: "absolute", 
+                  top:      -10000,
+                  left:     -10000,
+                  backgroundImage: starFieldImage 
+                }} />
+              </Draggable>
             </div>
         );
     }
